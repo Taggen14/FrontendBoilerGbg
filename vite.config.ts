24b6fb380ec85b30cd8fda8184d1ca9a43226f66
Jens Bengtsson <jb225je@student.lnu.se>
2024-11-04 15:22:18 +0100

    new viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  define: {
+    'process.env': {
+      REACT_APP_ENVIRONMENT: JSON.stringify(process.env.REACT_APP_ENVIRONMENT)
+    }
+  }
 })
