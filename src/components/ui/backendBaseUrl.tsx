bc643a5baf85b20c751a1ee17415b0e6dc879497
Jens Bengtsson <jb225je@student.lnu.se>
2024-11-04 15:13:18 +0100

    prod env var

--- src/components/ui/backendBaseUrl.tsx
+++ src/components/ui/backendBaseUrl.tsx
@@ -1,10 +1,8 @@
 const environment = process.env.REACT_APP_ENVIRONMENT;
 // Now, you can use the 'environment' variable in your code
 let backendBaseUrl = "backend-boiler-gbg-git-main-jens-projects-35f91083.vercel.app";
-if (environment === "prod") {
-  backendBaseUrl = "backend-boiler-gbg-git-main-jens-projects-35f91083.vercel.app";
-} else {
+if (environment !== "prod") {
   backendBaseUrl = "http://localhost:1337";
-}
+} 
 
 export default backendBaseUrl;
